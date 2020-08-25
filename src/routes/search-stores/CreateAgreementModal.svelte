733c50ba7741c946ebb454b65789e75cbbb64170
Noël Mugnier <noel.mugnier@outlook.com>
2020-08-25 12:19:27 +0200

    Change - Correction de la gestion de la création d'agreements pour producers et stores

--- src/routes/search-stores/CreateAgreementModal.svelte
+++ src/routes/search-stores/CreateAgreementModal.svelte
@@ -1,69 +1,75 @@
 <script>
-  import { faCheck } from "@fortawesome/free-solid-svg-icons";;
-  import SelectTime from "./SelectTime.svelte";
-  import ActionConfirm from "./../../components/modal/ActionConfirm.svelte";
-  import GetGraphQLInstance from "./../../services/SheaftGraphQL.js";
-  import { CREATE_AGREEMENT } from "./mutations.js";
-  import SheaftErrors from "./../../services/SheaftErrors";
+	import { faCheck } from "@fortawesome/free-solid-svg-icons";
+	import SelectTime from "./SelectTime.svelte";
+	import ActionConfirm from "./../../components/modal/ActionConfirm.svelte";
+	import GetGraphQLInstance from "./../../services/SheaftGraphQL.js";
+	import { CREATE_AGREEMENT } from "./mutations.js";
+	import SheaftErrors from "./../../services/SheaftErrors";
+	import { GET_AGREEMENTS } from "./queries";
 
-  const errorsHandler = new SheaftErrors();
+	const errorsHandler = new SheaftErrors();
 
-  export let onClosed, close, store;
+	export let onClosed, close, store;
 
-  const graphQLInstance = GetGraphQLInstance();
+	const graphQLInstance = GetGraphQLInstance();
 
-  let selectedDelivery = null;
-  let isLoading = false;
-  let isValid = false;
+	let selectedDelivery = null;
+	let isLoading = false;
+	let isValid = false;
 
-  $: isValid = selectedDelivery && selectedDelivery.selectedHours.length > 0;
+	$: isValid = selectedDelivery && selectedDelivery.selectedHours.length > 0;
 
-  const handleSubmit = async () => {
-    isLoading = true;
-    var res = await graphQLInstance.mutate(CREATE_AGREEMENT, {
-      storeId: store.id,
-      selectedHours: selectedDelivery.selectedHours.map((selectedHour) => {
-        return {
-          days: [selectedHour.day],
-          from: selectedHour.from,
-          to: selectedHour.to
-        }
-      }),
-      deliveryModeId: selectedDelivery.deliveryId
-    });
+	const handleSubmit = async () => {
+		isLoading = true;
+		var res = await graphQLInstance.mutate(
+			CREATE_AGREEMENT,
+			{
+				storeId: store.id,
+				selectedHours: selectedDelivery.selectedHours.map((selectedHour) => {
+					return {
+						days: [selectedHour.day],
+						from: selectedHour.from,
+						to: selectedHour.to,
+					};
+				}),
+				deliveryModeId: selectedDelivery.deliveryId,
+			},
+			errorsHandler.Uuid,
+			GET_AGREEMENTS
+		);
 
-    isLoading = false;
+		isLoading = false;
 
-    if (!res.success) {
-      // todo
-      return;
-    }
+		if (!res.success) {
+			// todo
+			return;
+		}
 
-    return await closeModal(res);
-  }
+		return await closeModal(res);
+	};
 
-  async function closeModal(obj) {
-    close();
-    if (onClosed) await onClosed(obj);
-  }
+	async function closeModal(obj) {
+		close();
+		if (onClosed) await onClosed(obj);
+	}
 </script>
 
 <ActionConfirm
-  {errorsHandler}
-  title='Demander un accord'
-  level="success"
-  icon={faCheck}
-  submit={handleSubmit}
-  {isLoading}
-  valid={isValid}
-  submitText="Confirmer"
-  closeText="Annuler"
-  {close}>
-  <p class="leading-5">
-    Vous vous apprêtez à demander un accord de partenariat avec {store.name}
-  </p>
-  <p class="leading-5">
-    Le magasin sera automatiquement notifié de votre demande.
-  </p>
-  <SelectTime bind:selectedDelivery={selectedDelivery} />
+	{errorsHandler}
+	title="Demander un accord"
+	level="success"
+	icon={faCheck}
+	submit={handleSubmit}
+	{isLoading}
+	valid={isValid}
+	submitText="Confirmer"
+	closeText="Annuler"
+	{close}>
+	<p class="leading-5">
+		Vous vous apprêtez à demander un accord de partenariat avec {store.name}
+	</p>
+	<p class="leading-5">
+		Le magasin sera automatiquement notifié de votre demande.
+	</p>
+	<SelectTime bind:selectedDelivery />
 </ActionConfirm>
