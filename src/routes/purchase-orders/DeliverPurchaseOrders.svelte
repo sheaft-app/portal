76ead50769d88c41b3508bd8f36cb3a3a582fc81
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2020-10-24 17:59:45 +0200

    Update -  Wording

--- src/routes/purchase-orders/DeliverPurchaseOrders.svelte
+++ src/routes/purchase-orders/DeliverPurchaseOrders.svelte
@@ -51,6 +51,6 @@
 	close={() => handleClose({ success: false, data: null })}>
 	<p class="leading-5">
 		Marquer {purchaseOrders.length > 1 ? 'ces commandes comme livrées' : 'cette commande comme livrée'}
-		confirmera au client qu'il a bien récupéré sa commande.
+		indiquera au client que la commande a été réceptionnée.
 	</p>
 </ActionConfirm>
