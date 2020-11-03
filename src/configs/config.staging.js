baf10bc5e54db158a8bed7081f431f13e8d0a3f6
Noël Mugnier <noel.mugnier@outlook.com>
2020-11-03 19:43:37 +0100

    Fix - Correction des urls pour l'app staging

--- src/configs/config.staging.js
+++ src/configs/config.staging.js
@@ -1,18 +1,18 @@
 export const config = {
 	env: "production",
-	front: "https://sheaftapprd.z6.web.core.windows.net",
+	front: "https://rd.app.sheaft.com",
 	api: "https://sheaft-api-rd.azurewebsites.net",
 	signalr: "https://sheaft-signalr-rd.azurewebsites.net",
 	auth: {
 		url: "https://sheaft-identity-rd.azurewebsites.net",
 		settings: {
 			client_id: "37f6e39f-5b99-4943-b906-a033bdf2f1bb",
-			redirect_uri: "https://sheaftapprd.z6.web.core.windows.net/#/callback",
-			post_logout_redirect_uri: "https://sheaftapprd.z6.web.core.windows.net",
+			redirect_uri: "https://rd.app.sheaft.com/#/callback",
+			post_logout_redirect_uri: "https://rd.app.sheaft.com",
 			response_type: "code",
 			scope: "openid profile email crud role offline_access",
 			authority: "https://sheaft-identity-rd.azurewebsites.net",
-			silent_redirect_uri: "https://sheaftapprd.z6.web.core.windows.net/#/callback-silent",
+			silent_redirect_uri: "https://rd.app.sheaft.com/#/callback-silent",
 			automaticSilentRenew: true,
 			loadUserInfo: true,
 			filterProtocolClaims: true,
