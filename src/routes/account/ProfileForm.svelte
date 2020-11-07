35835f28d16591c3312f6a5aa91740433a6c39e9
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2020-11-07 23:14:29 +0100

    Reset - Déconnexion brutale à l'update du profil

--- src/routes/account/ProfileForm.svelte
+++ src/routes/account/ProfileForm.svelte
@@ -80,6 +80,9 @@
       notificationsInstance.success(
         "Vos modifications ont bien été appliquées."
       );
+
+      await authInstance.loginSilent();
+      await loginFreshdesk();
 		}
   };
 
