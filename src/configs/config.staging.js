4349a515e8cbb8aa87bf95d69d7d66ee1aba0b59
Noël Mugnier <noel.mugnier@outlook.com>
2020-11-11 18:01:38 +0100

    Update - Modification de l'url de rd vers dev

--- src/configs/config.staging.js
+++ src/configs/config.staging.js
@@ -1,18 +1,18 @@
 export const config = {
 	env: "production",
-	front: "https://rd.app.sheaft.com",
+	front: "https://dev.app.sheaft.com",
 	api: "https://sheaft-api-rd.azurewebsites.net",
 	signalr: "https://sheaft-signalr-rd.azurewebsites.net",
 	auth: {
 		url: "https://sheaft-identity-rd.azurewebsites.net",
 		settings: {
 			client_id: "37f6e39f-5b99-4943-b906-a033bdf2f1bb",
-			redirect_uri: "https://rd.app.sheaft.com/#/callback",
+			redirect_uri: "https://dev.app.sheaft.com/#/callback",
 			post_logout_redirect_uri: "https://rd.app.sheaft.com",
 			response_type: "code",
 			scope: "openid profile email crud role offline_access",
 			authority: "https://sheaft-identity-rd.azurewebsites.net",
-			silent_redirect_uri: "https://rd.app.sheaft.com/#/callback-silent",
+			silent_redirect_uri: "https://dev.app.sheaft.com/#/callback-silent",
 			automaticSilentRenew: true,
 			loadUserInfo: true,
 			filterProtocolClaims: true,
