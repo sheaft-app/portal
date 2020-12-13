5a3b0c5856dd50f391fa43cdd408279e84d12357
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2020-12-13 16:50:48 +0100

    Fix - utilisation de la lat/lng dans les liens gmaps

--- src/helpers/app.js
+++ src/helpers/app.js
@@ -105,7 +105,7 @@ export const removeKeys = (obj, keys) => {
 }
 
 export const encodeQuerySearchUrl = address => {
-	return address ? encodeURI(`${address.line1} ${address.line2 ? address.line2 : ""} ${address.zipcode}`) : '';
+	return address ? encodeURI(`${address.latitude}+${address.longitude}`) : '';
 };
 
 export const freezeBody = () => {
