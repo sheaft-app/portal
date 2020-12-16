99ee83edd1f4ad764151c65e330feeb119436205
Noël Mugnier <noel.mugnier@outlook.com>
2020-12-16 18:30:38 +0100

    Fix - Correction du purgecss lors des build de production suite à la montée de version 3.x

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@ const tailwind = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const postcssImport = require("postcss-import");
 const purgecss = require("@fullhuman/postcss-purgecss")({
-	whitelist: ['text-blue-500', 'bg-blue-500', 'text-green-500', 'bg-green-500', 'text-red-500','bg-red-500', 'text-gray-600', 'bg-gray-600'], // les orderStatusKind générés dynamiquement
-	whitelistPatterns: [/svelte/, /fa/, /notyf/, /leaflet/, /ssp-/, /scal-/],
+	safelist: [/svelte/, /fa/, /notyf/, /leaflet/, /ssp-/, /scal-/, 'text-blue-500', 'bg-blue-500', 'text-green-500', 'bg-green-500', 'text-red-500','bg-red-500', 'text-gray-600', 'bg-gray-600'],		
 	content: ["./src/**/*.html", "./src/**/*.svelte"],
 	defaultExtractor: (content) => {
 		const regExp = new RegExp(/[\w-/:]+(?<!:)/g);
