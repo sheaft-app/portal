dc0cbd1595900781630a8d00a075ac15db85f23c
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-03-25 21:15:28 +0100

    Update - Déplacement du bouton "Fermer"

--- src/routes/external/ProducerReadMoreModal.svelte
+++ src/routes/external/ProducerReadMoreModal.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div class="px-6 pt-8" style="max-width: 650px;">
+  <button type="button" on:click={close} class="btn btn-lg mb-5 border border-gray-400">Fermer</button>
   <h2 class="font-semibold">À propos de {producer.name}</h2>
   <span class="bg-primary h-1 w-20 block mt-2 mb-4"></span>
   {#each producer.description.match(/[^\n]+/gm) as line}
     <p>{line}</p>
   {/each}
-  <button type="button" on:click={close} class="btn btn-lg mt-5 mb-5 border border-gray-400">Fermer</button>
 </div>
 
 <style lang="scss">
