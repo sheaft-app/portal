a6e13409e9419be56a0a25e5c3715243687ec6ec
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-04-05 16:31:42 +0200

    Fix - Ajout panier détails produit

--- src/components/controls/AddToCart.svelte
+++ src/components/controls/AddToCart.svelte
@@ -1,17 +1,16 @@
 <script>
-  import { onMount } from "svelte";
   import ProductCartQuantity from "./ProductCartQuantity.svelte";
   import cart from "./../../stores/cart";
   import Icon from "svelte-awesome";
-  import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+  import { faShoppingCart, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
   export let product, disabled, showIsInCartInfo = true;
 
   let state = 0;
   
-  const addToCart = () => {
-    if (disabled) return;
-    cart.addProduct(product.id);
+  const addToCart = (_productId) => {
+    if (disabled || $cart.isSaving) return;
+    cart.updateProduct(_productId, 1);
   }
 
   $: state = $cart.products.find((i) => i.id == product.id) ? 1 : 0;
@@ -24,8 +23,12 @@
   />
   <p class="text-green-500 text-sm">Dans le panier</p>
 {:else}
-  <button class="btn btn-lg btn-accent" class:disabled {disabled} type="button" on:click={addToCart}>
-    <Icon data={faShoppingCart} class="mr-2 hidden md:inline" />
+  <button class="btn btn-lg btn-accent" class:disabled={disabled || $cart.isSaving} disabled={disabled || $cart.isSaving} type="button" on:click={() => addToCart(product.id)}>
+    {#if $cart.isSaving}
+      <Icon data={faSpinner} spin class="mr-2 hidden md:inline" />
+    {:else}
+      <Icon data={faShoppingCart} class="mr-2 hidden md:inline" />
+    {/if}
     Ajouter au panier
   </button>
 {/if}
