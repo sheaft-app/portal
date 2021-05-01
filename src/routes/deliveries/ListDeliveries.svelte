00f2d4637f71144d98bd1622cce09b7fc84a9f81
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-05-01 16:50:55 +0200

    Fix - Typo

--- src/routes/deliveries/ListDeliveries.svelte
+++ src/routes/deliveries/ListDeliveries.svelte
@@ -24,7 +24,7 @@
 	const actions = [
 		{
 			click: () => routerInstance.goTo(DeliveryRoutes.Create),
-			text: "Créer un créaneau de livraison",
+			text: "Créer un créneau de livraison",
 			icon: faPlus,
 			color: "green",
 		}
