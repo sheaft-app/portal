df66d3443d1d0703f670bfef0b741a649e2423e9
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-05-29 18:30:47 +0200

    Fix - Retour des boutons Actif/Historique

--- src/routes/my-orders/ListMyOrders.svelte
+++ src/routes/my-orders/ListMyOrders.svelte
@@ -95,67 +95,67 @@
 	$: hiddenNavigation =
 		$items.filter((o) => !o.active).length < 1 ||
 		$items.filter((o) => o.active).length < 1;
-
-	$: actions = [
-		{
-			click: () => selectOrdersDisplay('active'),
-			text: "Actives",
-			icon: faPlay,
-			class: selected === 'active' ? 'selected' : '',
-			color: "green",
-		},
-		{
-			click: () => selectOrdersDisplay('passed'),
-			text: "Historique",
-			icon: faClock,
-			color: "green",
-			class: selected === 'passed' ? 'selected' : '',
-			disabled: isLoading
-		}
-	]
 </script>
 
 <TransitionWrapper>
 	<PageHeader name="Mes commandes"/>
 	<PageBody {errorsHandler} noResultsPage={SearchProductRoutes.NoResults} {isLoading} noResults={$items.length < 1 && validatingOrders.length < 1}>
-			<Actions {actions} show={!hiddenNavigation}/>
-			<div class="mb-5">
-				{#each validatingOrders as validatingOrder}
-					{#if validatingOrder.status == OrderStatus.Waiting.value}
-						<div class="bg-white shadow px-4 py-3 mb-3">
-							<div class="flex items-center mb-2">
-								<Icon data={faCircleNotch} class="text-green-500 mr-3" spin/>
-								<p class="font-semibold text-green-500">Un paiement est en cours de traitement</p>
-							</div>
-							<p>Le paiement de votre panier de <b>{formatMoney(validatingOrder.totalPrice)}</b> est en cours de
-								traitement. Dès que le paiement aura été traité, la commande apparaîtra ici.</p>
-							<p>Vous n'avez rien à faire. On vous fait signe dès qu'il arrive quoi que ce soit !</p>
-						</div>
-					{:else}
-						<div class="bg-white shadow px-4 py-3 mb-3">
-							<div class="flex items-center mb-2">
-								<Icon data={faCircleNotch} class="text-green-500 mr-3" spin/>
-								<p class="font-semibold text-green-500">Votre paiement a été validé</p>
-							</div>
-							<p>Le paiement de votre panier de <b>{formatMoney(validatingOrder.totalPrice)}</b> a bien été traité. La
-								commande correspondante est en cours d'envoi au producteur et apparaîtra d'ici quelques instants.</p>
-							<p>Vous n'avez rien à faire. On vous fait signe dès que c'est envoyé !</p>
-						</div>
-					{/if}
-				{/each}
+		{#if !hiddenNavigation}
+			<div
+				class="text-lg justify-center button-group mb-5 w-full md:w-auto -mx-1 md:mx-0">
+				<button
+					on:click={() => selectOrdersDisplay('active')}
+					type="button"
+					class:selected={selected === 'active'}
+					class:disabled={isLoading}>
+					Actives
+				</button>
+				<button
+					on:click={() => selectOrdersDisplay('passed')}
+					type="button"
+					class:selected={selected === 'passed'}
+					class:disabled={isLoading}>
+					Historique
+				</button>
 			</div>
-			<div class="-mx-4 md:mx-0 md:overflow-x-auto md:w-full">
-				<div
-					class="align-middle inline-block min-w-full overflow-hidden items"
-					style="padding-left: 1px; padding-right: 1px;">
-					<div
-						class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 md:gap-3 -mx-4
-					md:mx-0 mb-10">
-						{#each orders as order, index}
-							<MyOrderListItem {order}/>
-						{/each}
+		{/if}
+		<div class="mb-5">
+			{#each validatingOrders as validatingOrder}
+				{#if validatingOrder.status == OrderStatus.Waiting.value}
+					<div class="bg-white shadow px-4 py-3 mb-3">
+						<div class="flex items-center mb-2">
+							<Icon data={faCircleNotch} class="text-green-500 mr-3" spin/>
+							<p class="font-semibold text-green-500">Un paiement est en cours de traitement</p>
+						</div>
+						<p>Le paiement de votre panier de <b>{formatMoney(validatingOrder.totalPrice)}</b> est en cours de
+							traitement. Dès que le paiement aura été traité, la commande apparaîtra ici.</p>
+						<p>Vous n'avez rien à faire. On vous fait signe dès qu'il arrive quoi que ce soit !</p>
 					</div>
+				{:else}
+					<div class="bg-white shadow px-4 py-3 mb-3">
+						<div class="flex items-center mb-2">
+							<Icon data={faCircleNotch} class="text-green-500 mr-3" spin/>
+							<p class="font-semibold text-green-500">Votre paiement a été validé</p>
+						</div>
+						<p>Le paiement de votre panier de <b>{formatMoney(validatingOrder.totalPrice)}</b> a bien été traité. La
+							commande correspondante est en cours d'envoi au producteur et apparaîtra d'ici quelques instants.</p>
+						<p>Vous n'avez rien à faire. On vous fait signe dès que c'est envoyé !</p>
+					</div>
+				{/if}
+			{/each}
+		</div>
+		<div class="-mx-4 md:mx-0 md:overflow-x-auto md:w-full">
+			<div
+				class="align-middle inline-block min-w-full overflow-hidden items"
+				style="padding-left: 1px; padding-right: 1px;">
+				<div
+					class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 md:gap-3 -mx-4
+				md:mx-0 mb-10">
+					{#each orders as order, index}
+						<MyOrderListItem {order}/>
+					{/each}
 				</div>
 			</div>
+		</div>
 	</PageBody>
 </TransitionWrapper>
