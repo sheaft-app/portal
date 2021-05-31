cc1cceaaf65943372459664a4113381d93026514
Noël Mugnier <noel.mugnier@outlook.com>
2021-05-31 17:15:01 +0200

    DLF276 - Le catalogue ou les pictures ne sont pas requises

--- src/routes/products/productForm.js
+++ src/routes/products/productForm.js
@@ -14,13 +14,14 @@ export const initialValues = {
     tags: [],
     available: true,
     catalogs:[],
+		pictures:[],
     producer: {
         notSubjectToVat: false
     }
 };
 
 export const validators = (product) => ({
-    ...getDefaultFields(product, initialValues, ['catalogs','reference', 'returnableId', 'description', 'picture']),
+    ...getDefaultFields(product, initialValues, ['pictures', 'catalogs', 'reference', 'returnableId', 'description', 'picture']),
     name: {
         value: product.name,
         validators: ["required", "min:3"],
@@ -42,18 +43,18 @@ export const validators = (product) => ({
 export const normalizeCreateProduct = product => omit({
         ...product,
         returnableId: product.returnable ? product.returnable.id : null,
-        catalogs: product.catalogs.map((c) => ({
+        catalogs: product.catalogs ? product.catalogs.map((c) => ({
             id: c.id,
             wholeSalePricePerUnit: c.wholeSalePricePerUnit
-        })),
-				pictures: product.pictures.map(p => ({id: p.new ? null : p.id, data: p.new ? p.data : null, position: p.position})),
+        })) : [],
+				pictures: product.pictures ? product.pictures.map(p => ({id: p.new ? null : p.id, data: p.new ? p.data : null, position: p.position})) : [],
         tags: product.tags.map(i => i.id)
     }, ['producer', 'createdOn', 'rating', 'ratingsCount', 'returnable', 'weight']);
 
 export const normalizeUpdateProduct = product => ({
     ...normalizeCreateProduct(product),
-    catalogs: product.catalogs.filter((c) => !c.markForDeletion).map((c) => ({
+    catalogs: product.catalogs ? product.catalogs.filter((c) => !c.markForDeletion).map((c) => ({
         id: c.id,
         wholeSalePricePerUnit: c.wholeSalePricePerUnit
-    }))
+    })) : []
 })
