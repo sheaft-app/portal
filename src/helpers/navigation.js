f2e918c40d168ea19cb979769a0f8cd5c57689aa
Noël Mugnier <noel.mugnier@outlook.com>
2021-06-09 20:41:55 +0200

    DLF316 - Correction de la redirection sur home pendant l'enregistrement

--- src/helpers/navigation.js
+++ src/helpers/navigation.js
@@ -2,8 +2,14 @@ import Roles from "./../enums/Roles";
 import SearchProductRoutes from "../routes/search-products/routes";
 import ProductRoutes from "../routes/products/routes";
 import QuickOrderRoutes from "../routes/quick-orders/routes";
+import RegisterRoutes from "../routes/register/routes";
 
 export const goToHome = (authManager, routerInstance) => {
+	if (authManager.authenticated && !authManager.registered) {
+		routerInstance.goTo(RegisterRoutes.Choose);
+		return;
+	}
+
 	if (authManager.isInRole([Roles.Producer.Value])) {
 		routerInstance.goTo(ProductRoutes.List);
 		return;
