7acd465ae0e65479379c764aeaf89145c72f794b
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-07-07 18:06:10 +0200

    Update - Missing mutation when not rescheduling deliveryBatch

--- src/routes/deliveryBatches/process/EndDelivery.svelte
+++ src/routes/deliveryBatches/process/EndDelivery.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { GET_DELIVERY_BATCH_DETAILS } from "../queries";
+    import { GET_DELIVERY_BATCH_DETAILS, GET_DELIVERY_BATCHES } from "../queries";
     import { COMPLETE_DELIVERY_BATCH } from "../mutations";
 	import { onMount, getContext } from "svelte";
 	import SheaftErrors from "../../../services/SheaftErrors";
@@ -8,16 +8,19 @@
     import DeliveryStatus from "../../../enums/DeliveryStatus";
     import SimpleStoreCard from "../../deliveries/SimpleStoreCard.svelte";
     import PostponeDeliveryBatchModal from "../PostponeDeliveryBatchModal.svelte";
+    import Icon from "svelte-awesome";
+    import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
     
     export let params = {};
 
-    const { query } = getContext("api");
+    const { query, mutate } = getContext("api");
     const { open } = getContext("modal");
     const errorsHandler = new SheaftErrors();
     const routerInstance = GetRouterInstance();
     
     let skippedDeliveries = [];
     let deliveryBatch;
+    let isSubmitting = false;
     let isLoading = true;
 
     onMount(async () => {
@@ -32,6 +35,20 @@
         skippedDeliveries = deliveryBatch.deliveries.filter((d) => d.status == DeliveryStatus.Skipped.Value);
 		isLoading = false;
     });
+
+    const completeDelivery = async () => {
+        isSubmitting = true;
+        await mutate({
+			mutation: COMPLETE_DELIVERY_BATCH,
+			variables: { id: params.id },
+			errorsHandler,
+			success: (res) => routerInstance.goTo(DeliveryBatchesRoutes.Summary, { id: params.id }),
+			successNotification: "Livraison terminée avec succès !",
+			errorNotification: "Impossible de terminer la livraison",
+			clearCache: [GET_DELIVERY_BATCHES, GET_DELIVERY_BATCH_DETAILS],
+		});
+        isSubmitting = false;
+    }
 </script>
     
 {#if isLoading}
@@ -60,7 +77,12 @@
                 Reprogrammer
             </button>
         {:else}
-            <button on:click={() => routerInstance.goTo(DeliveryBatchRoutes.List)} class="block btn btn-lg btn-accent justify-center w-full">Terminer</button>
+            <button on:click={completeDelivery} disabled={isSubmitting} class:disabled={isSubmitting} class="block btn btn-lg btn-accent justify-center w-full">
+                {#if isSubmitting}
+                    <Icon data={faCircleNotch} spin class="mr-2" />
+                {/if}
+                Terminer
+            </button>
         {/if}
     </div>
 {/if}
