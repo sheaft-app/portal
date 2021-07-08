da2dc5433776d37589a1e1b0ac0d39c00ff7e11e
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-07-08 19:56:36 +0200

    Fix - Flex jump

--- src/routes/deliveryBatches/DeliveryBatchItem.svelte
+++ src/routes/deliveryBatches/DeliveryBatchItem.svelte
@@ -26,8 +26,8 @@
 
 <div class="delivery-card rounded-3xl px-4 py-2 bg-white">
 	<div class="flex justify-between py-2 border-b border-gray-300 flex-wrap flex-col-reverse xl:flex-nowrap xl:flex-row">
-		<div>
-			<p class="font-semibold text-xl" style="max-width: 300px;">{deliveryBatch.name}</p>
+		<div style="max-width: 190px;">
+			<p class="font-semibold text-xl">{deliveryBatch.name}</p>
 			<p>{format(new Date(deliveryBatch.scheduledOn), "PPPP", { locale: fr })}</p>
 			<p>débute à {from.hours}h{from.minutes == 0 ? "00" : from.minutes}</p>
 		</div>
