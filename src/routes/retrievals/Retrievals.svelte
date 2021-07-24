12c1f9284195ebbaa9091389a1e65ddf1cdabee5
Noël Mugnier <46538060+noelmugnier@users.noreply.github.com>
2021-07-24 19:32:47 +0200

    DLF420 - Ajout d'un module pour lister les commandes à distribuer (#48)

---
+++ src/routes/retrievals/Retrievals.svelte
@@ -0,0 +1,19 @@
+<script>
+	import RetrievalRoutes from "./routes.js";
+	import ListRetrievals from "./ListRetrievals.svelte";
+	import ListHistoryRetrievals from "./ListHistoryRetrievals.svelte";
+	import RetrievalDetails from "./RetrievalDetails.svelte";
+	import Router from "svelte-spa-router";
+	import Meta from "../../components/Meta.svelte";
+
+	export let params = {};
+	export const prefix = RetrievalRoutes.Prefix;
+
+	const routes = {};
+	routes[RetrievalRoutes.List.SubPart] = ListRetrievals;
+	routes[RetrievalRoutes.History.SubPart] = ListHistoryRetrievals;
+	routes[RetrievalRoutes.Details.SubPart] = RetrievalDetails;
+</script>
+
+<Meta />
+<Router {routes} {prefix} />
