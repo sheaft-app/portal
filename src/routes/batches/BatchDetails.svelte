c8d5695cf659f4f1e4fe1da3200c35f2434ef3c7
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2021-07-26 18:13:13 +0200

    New - Allow batch edit

--- src/routes/batches/BatchDetails.svelte
+++ src/routes/batches/BatchDetails.svelte
@@ -1,26 +1,34 @@
 <script>
-	import { GET_BATCH_DETAILS } from "./queries";
+	import { GET_BATCHES, GET_BATCH_DETAILS } from "./queries";
 	import { onMount, getContext } from "svelte";
+	import { getIsoDate } from "../../helpers/app";
 	import TransitionWrapper from "../../components/TransitionWrapper.svelte";
 	import PageHeader from "../../components/PageHeader.svelte";
 	import PageBody from "../../components/PageBody.svelte";
 	import SheaftErrors from "../../services/SheaftErrors";
+	import Icon from "svelte-awesome";
+	import { faPaperPlane, faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 	import GetRouterInstance from "../../services/SheaftRouter";
 	import BatchesRoutes from "./routes";
 	import format from "date-fns/format";
 	import fr from "date-fns/locale/fr";
-	import { timeSpanToTime } from "../../helpers/app";
+	import { UPDATE_BATCH } from "./mutations";
+	import DatePickerWrapper from "./../../components/controls/DatePickerWrapper.svelte";
 	import Observations from "../../components/observations/Observations.svelte";
 
 	export let params = {};
 
-	const { query } = getContext("api");
+	const { query, mutate } = getContext("api");
 	const errorsHandler = new SheaftErrors();
 	const routerInstance = GetRouterInstance();
 
 	let batch;
 	let displayObservationsPanel = false;
 	let isLoading = true;
+	let isSubmitting = false;
+
+	let dlcOrDluo = 'dlc'; 
+	let expirationDate = new Date();
 
 	onMount(async () => {
 		isLoading = true;
@@ -31,9 +39,29 @@
 			error: () => routerInstance.goTo(BatchesRoutes.History),
 			errorNotification: "Impossible de trouver le lot.",
 		});
-		console.log(batch);
+		dlcOrDluo = batch.dlc ? 'dlc' : 'ddm';
+		expirationDate = batch.dlc ? new Date(batch.dlc) : new Date(batch.ddm);
+
 		isLoading = false;
 	});
+
+	const handleSubmit = async () => {
+		isSubmitting = true;
+		await mutate({
+			mutation: UPDATE_BATCH,
+			variables: {
+				id: batch.id,
+				number: batch.number,
+				dlc: dlcOrDluo == "dlc" ? getIsoDate(expirationDate) : null,
+				ddm: dlcOrDluo == "ddm" ? getIsoDate(expirationDate) : null,
+			},
+			errorsHandler,
+			successNotification: "Lot modifié avec succès",
+			errorNotification: "Impossible de modifier le lot",
+			clearCache: [GET_BATCHES],
+		});
+		isSubmitting = false;
+	};
 </script>
 
 <TransitionWrapper>
@@ -46,13 +74,13 @@
 		{#if batch.dlc}
 			<p>DLC : {format(new Date(batch.dlc), "PPPP", { locale: fr })}</p>
 		{/if}
-		{#if batch.dluo}
+		{#if batch.ddm}
 			<p>DDM : {format(new Date(batch.ddm), "PPPP", { locale: fr })}</p>
 		{/if}
 		{#if batch.observations.length > 0}
 			<div
 				class="py-5 px-3 md:px-8 overflow-x-auto -mx-4 md:mx-0 bg-blue-100
-				shadow md:rounded md:mb-2 mt-3"
+				shadow md:rounded md:mb-3 mt-3"
 			>
 				<p class="uppercase font-bold leading-none">Des observations ont été remontées !</p>
 				<div class="mt-2">
@@ -62,6 +90,54 @@
 				</div>
 			</div>
 		{/if}
+		<div class="form-control text-left mt-3">
+			<label for="batch">Numéro du lot *</label>
+			<input bind:value={batch.number} id="batch" type="text" />
+		</div>
+		<div class="form-control">
+			<div class="flex w-full">
+				<div class="w-full">
+					<div class="w-full text-lg justify-center button-group">
+						<button
+							on:click={() => (dlcOrDluo = "dlc")}
+							type="button"
+							class="text-sm md:text-base"
+							class:selected={dlcOrDluo == "dlc"}
+							class:skeleton-box={isLoading || isSubmitting}
+						>
+							DLC
+						</button>
+						<button
+							on:click={() => (dlcOrDluo = "ddm")}
+							type="button"
+							class="text-sm md:text-base"
+							class:selected={dlcOrDluo == "ddm"}
+							class:skeleton-box={isLoading || isSubmitting}
+						>
+							DDM
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="form-control text-left" style="display:block;">
+			<label>{dlcOrDluo == "dlc" ? "DLC *" : "DDM *"}</label>
+			<DatePickerWrapper bind:selected={expirationDate} dateChosen={true} />
+		</div>
+
+		<small>* champs requis</small>
+		<div class="form-control mt-5">
+			<button
+				type="button"
+				disabled={isLoading || isSubmitting}
+				class:disabled={isLoading ||isSubmitting}
+				on:click={handleSubmit}
+				class="btn btn-primary btn-xl justify-center w-full md:w-auto"
+			>
+				<Icon data={isSubmitting ? faCircleNotch : faPaperPlane} class="mr-2 inline" spin={isSubmitting} />
+				Valider
+			</button>
+		</div>
 		{#each batch.products as product}
 			<div
 				class="bg-white overflow-hidden rounded-lg p-3 lg:p-6 shadow flex
